50dd67d81ebeefc26f53509bcc1d976b1bbddc05
Mark Song <marksong@augmentum.com.cn>
2016-12-06 11:51:45 +0800

    Initialize project.

---
+++ SRMAPIHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SRMAPIHelper
+//
+//  Created by marksong on 12/6/16.
+//  Copyright © 2016 S.R. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
